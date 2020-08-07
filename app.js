7bbe6afb99600301a1e3299a66472d071af64696
Abhijeet11187 <lokhandeabhijeet11187@gmail.com>
2020-08-07 17:45:36 +0530

    Handling the error in the app.js file

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 const productsRoutes = require('./API/Routes/product');
-const orderRoutes=require('./API/Routes/order');
-const app=express();
+const orderRoutes = require('./API/Routes/order');
+const app = express();
 
 console.log("In the app.js ");
 app.use(morgan('dev'));
-app.use('/products',productsRoutes);
-app.use('/order',orderRoutes);
+app.use('/products', productsRoutes);
+app.use('/order', orderRoutes);
+
+// Handling the error 
+
+app.use((req, res, next) => {
+    const error = new Error('URL not found');
+    error.status = 404;
+    console.log("Error status code is set");
+    next(error);
+})
+app.use((error,req, res, next) => {
+    console.log("In the next hanling")
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 // app.use((req,res,next)=>{
 //     res.status(200).json({
 //         asd:"sda"
 //     })
 // })
-module.exports=app;
+module.exports = app;
