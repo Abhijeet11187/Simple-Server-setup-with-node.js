723638f55b58e201276aac52cee432ea4d0b706d
Abhijeet11187 <lokhandeabhijeet11187@gmail.com>
2020-08-07 17:30:08 +0530

    Morgan added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const productsRoutes = require('./API/Routes/product');
 const orderRoutes=require('./API/Routes/order');
 const app=express();
 
 console.log("In the app.js ");
-
+app.use(morgan('dev'));
 app.use('/products',productsRoutes);
 app.use('/order',orderRoutes);
 // app.use((req,res,next)=>{
