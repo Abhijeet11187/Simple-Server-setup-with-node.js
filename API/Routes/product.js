c2848ebb608a006cfce7a46d749adf6473029408
Abhijeet11187 <lokhandeabhijeet11187@gmail.com>
2020-09-15 17:35:24 +0530

    MongoDB Atlas connection and CRUD Operations on Product routes

--- API/Routes/product.js
+++ API/Routes/product.js
@@ -1,27 +1,130 @@
 const express = require('express');
-
+const mongoose=require('mongoose');
+const Product=require('../Models/product');
 const router = express.Router();
 
 console.log("In the Product.js router file");
 
+
+//Find All Products 
+
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "In the get of Product js"
-    });
+    Product.find()
+    .exec()
+    .then((documents)=>{
+        res.status(200).json({
+           ' Products are':documents
+        })
+    })
+    .catch((err)=>{
+        res.status(500).json(
+            {
+                error:err
+            }
+        )
+    })
 });
+
+
+// Find by Id
 
 router.get('/:productId',(req,res,next)=>{
     const id=req.params.productId;
+  Product.findById(id)
+  .exec()
+  .then(result=>{
+ if(result){
+    res.status(200).json(
+        {
+            message:"Product find",
+            result:result
+        }
+        )
+ }else{
+     res.status(404).json({
+         Error:"No valid Entry Found"
+     })
+ }
+  })
+  .catch(err=>{
+      res.status(500).json(err);
+  })
+})
+
+
+// Posting the Products
+
+router.post('/',(req,res,next)=>{
+    console.log("In the try")
+    console.log("In the product post");
+    const product=new Product(
+     {
+        _id: new mongoose.Types.ObjectId(),
+        name:req.body.name,
+        price:req.body.price
+    }
+    );
+    console.log("Saving");
+    product.save().then(
+        result=>{
+            console.log("Successfull ",result);
+            res.status(200).json({
+                'result is':result
+            })
+        }
+    ).catch((err)=>{
+        console.log("Error while putting data ",err);
+        res.status(500).json({
+            message:"Error while Putting the data",
+            error:err
+        });
+    });    
+})
+
+//Update the Products
+
+router.patch('/:productId',(req,res,next)=>{
+const id=req.params.productId;
+const UpdateOperations={};
+        for(const ops of req.body)
+        {
+            console.log("PropnMa e is ",ops.propName)
+              UpdateOperations[ops.propName]=ops.value;
+        }
+ Product.update({_id:id},{
+     $set:UpdateOperations
+ })
+ .exec()
+ .then(result=>{
     res.status(200).json({
-          idis:"id is id",
-          id:id
+        Updated:result
+        });
+ })
+ .catch(err=>{
+     res.status(500).json({
+         'Error While Update':err
+     })
+ })
+
+});
+
+
+// Delete the product
+
+router.delete('/:productId',(req,res,next)=>{
+    const id=req.params.productId;
+    Product.deleteOne({_id:id})
+    .exec()
+    .then(result=>{
+        res.status(200).json({
+            'deleted Successfully':result
+        })
+    }
+    )
+    .catch(err=>{
+        res.status(500).json({error:err})
     })
 })
 
-router.post('/',(req,res,next)=>{
-    res.status(200).json({
-        message:"In the post of products"
-    })
-})
 
 module.exports=router;
--- app.js
+++ app.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
 const productsRoutes = require('./API/Routes/product');
 const orderRoutes = require('./API/Routes/order');
 const app = express();
 
 console.log("In the app.js ");
+
+// Connection to MongoDb
+
+const connectDB = async () => {
+    try {
+    const connection = await mongoose.connect(
+        'mongodb+srv://Abhi:'+process.env.mongooseATLAS_password+'@node-rest-shop-yakve.mongodb.net/test?retryWrites=true&w=majority',
+    {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+    }
+    )
+    console.log('MongoDB Server connected');
+    } catch (error) {
+    console.log('MongoDB error when connecting: ${error}');
+    }
+    }
+    connectDB()
+
+//+++++++++
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//CORS eror handling here (You cannot encounter CORS errors while using PostMan because Postman is not browser its just testing tool)
+//CORS errors happen when Cross Origin Request is made (Another Webpage cannot access to our API)
+
+app.use((req,res,next)=>{
+
+    //Two headers are added here to resolve CROS error
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    );
+
+    //Browser send OPTION req first when you want to send post request
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        // OPTIONS request is just for finding out which options we have so we have to return the response
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//Routes to handle the request
+
 app.use('/products', productsRoutes);
 app.use('/order', orderRoutes);
 
@@ -18,7 +69,7 @@ app.use((req, res, next) => {
     next(error);
 })
 app.use((error,req, res, next) => {
-    console.log("In the next hanling")
+    console.log("In the next handling")
     res.status(error.status || 500);
     res.json({
         error: {
